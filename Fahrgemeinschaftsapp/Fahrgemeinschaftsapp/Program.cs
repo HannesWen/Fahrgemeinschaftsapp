3fa2db1454766c0364d6e0ff91c7c038db209b25
Wengert, Johannes <johannes.wengert@tecalliance.net>
2022-09-29 08:02:16 +0200

    Initial Commit

---
+++ Fahrgemeinschaftsapp/Fahrgemeinschaftsapp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrgemeinschaftsapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fahrgemeinschaftsapp
+
+            // mehr Dimensionales Array laenge, hoehe &  (Koordinaten, Ortnamen und Kmanzahl bis Weikersheim)
+            // alle Benutzer geben ort ein (ort kleinstellen um fehler zu vermeiden), anhand von Ziel TecAlliance Weikersheim wird bestmögliche 4-5 Personen kombination berechnet.
+            // 
+
+
+
+        }
+    }
+}
---
+++ Fahrgemeinschaftsapp/Fahrgemeinschaftsapp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Fahrgemeinschaftsapp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TecAlliance")]
+[assembly: AssemblyProduct("Fahrgemeinschaftsapp")]
+[assembly: AssemblyCopyright("Copyright © TecAlliance 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f311163b-e831-4d06-b50d-227170b40a19")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
